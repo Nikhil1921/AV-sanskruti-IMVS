030b2f10bafcaed714a590a7873514d3a14f88ef
nishant-6578 <densetek.nishant@gmail.com>
2022-12-08 14:57:50 +0530

    changes done

--- application/views/pages/syllabus.php
+++ application/views/pages/syllabus.php
@@ -5,47 +5,22 @@
 
         <div class="row">
             <div class="col-12">
-                <h2 class="text-center mt-5">For Junior Students</h2>
+                <h2 class="text-center mt-5">Exam and Syllabus understanding</h2>
                 <div class="row justify-content-center ">
                     <div class="col-lg-12 col-md-12 col-sm-12 col-12">
                         <div class="volunteers_form">
-                            <h4>This is a scholarship program for students currently in grades VI through XI (2022-2023)
-                                who will be enrolled in grades VII through XII (2023-2024) the following academic year.
+                            <h4>This is a scholarship program for students who are now in grades VI through XI
+                                (2022-2023) and will be in grades VII through XII (2023-2024) the following
+                                academic year.
                             </h4>
-                            <h5>The scholarship exam and timetable are listed below:-</h5>
-                            <div class="table-responsive text-center mt-4">
-                                <table class="table table-bordered table-striped product-table">
-                                    <thead class="bg_thead">
-                                        <tr>
-                                            <th>Subjects</th>
-                                            <th>Marks</th>
-                                            <th>Time</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr class="bg_first">
-                                            <td>Grammar</td>
-                                            <td>50</td>
-                                            <td>30 mins</td>
-                                        </tr>
-                                        <tr class="bg_second">
-                                            <td>Maths</td>
-                                            <td>50</td>
-                                            <td>30 mins</td>
-                                        </tr>
-                                        <tr class="bg_first">
-                                            <td>Total</td>
-                                            <td>100</td>
-                                            <td>60 mins</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-
+                            <h5>Based on their study sequence and general knowledge of the subjects, students of any
+                                standard will be given a 100-mark MCQ-based question paper for Math, Science, and
+                                English
+                                subjects.
+                            </h5>
                             <h3 class="mt-4 mb-2"><strong>NOTE:-</strong></h3>
                             <h5 class="mb-0">Exam will be conducted online according to your Std on 26th January 2023
                             </h5>
-
                         </div>
                     </div>
                 </div>
@@ -133,9 +108,10 @@
                 <div class="row justify-content-center">
                     <div class="col-lg-12 col-md-12 col-sm-12 col-12">
                         <div class="volunteers_form">
-                            <h4>This is a scholarship program for students currently in Std. XII Commerce and Science
-                                (2022-2023) who will be enrolled in the graduation program (2023-2024) the following
-                                academic year.
+                            <h4>This scholarship program is designed for students in Std. XII in science and commerce
+                                who
+                                have a family income of less than Rs. 8 lakh and desire to study pharmacy, MBA, CA, CS,
+                                computer science, medicine, or engineering
                             </h4>
                             <h3 class="mb-2"><strong>NOTE:-</strong></h3>
                             <h5 class="mb-0">The syllabus will be updated in the first week of April, and the exam will
